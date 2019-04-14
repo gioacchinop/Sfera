22a596cc703fe39fe8b8263758fae1eac864d8ab
DANIELE <perri.1717817@studenti.uniroma1.it>
2019-04-14 22:49:10 +0200

    Grafico e integrazione

--- analysis/Event.h
+++ analysis/Event.h
@@ -16,24 +16,32 @@ class Event {
 
   Event() {
     event_ = 0;
-    canal_ = 0;
+    channel_ = 0;
+    npoints_ = 1024;
   }
 
-  Event(int event, int canal) {
+  Event( int event, int channel, int n ) {
     event_ = event;
-    canal_ = canal;
+    channel_ = channel;
+    npoints_ = n;
   }
 
   ~Event() {}
 
+  void setEvent( int event ) { event_   = event; }
+  void setChannel( int channel ) { channel_   = channel; }
+  void setNPoints(  int n  ) { npoints_ = n;     }
   
+  void pShapeHisto( float* pshape, std::string fileName );
+  double TRintegrate( double xlo, double xhi, double *integrand );
 
   
  private:
 
   int event_;
-  int canal_;
-
+  int channel_;
+  int npoints_;
+  
 };
 
 #endif
