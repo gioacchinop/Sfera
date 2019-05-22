cec862ff5e489c5f0769bb72bb4dd16f7b8397e0
Samuel Patrone <samuelpatrone@MacBook-Pro-di-Samuel.local>
2019-05-22 12:25:26 +0200

    fit picchi

--- analysis/DataTree.cpp
+++ analysis/DataTree.cpp
@@ -1,9 +1,11 @@
 #define DataTree_cxx
 #include "DataTree.h"
-#include <TH2.h>
+#include <TH1.h>
 #include <TStyle.h>
 #include <TCanvas.h>
 #include <TLegend.h>
+#include <TF1.h>
+#include <TFitResult.h>
 
 #include "Event.h"
 
@@ -52,19 +54,34 @@ void DataTree::Loop(std::string filename)
     double integral=0;
     double baseline = 0;
     int flag = 0;
-
+<<<<<<< HEAD
+    
     TCanvas* c1bis = new TCanvas( "c1bis", "multipads", 1200, 1200 );
-    TCanvas* canvcharge = new TCanvas( "canvcharge", "canvcharge", 900, 600 );
+    TCanvas* canvcharge = new TCanvas( "canvcharge", "canvcharge", 1200, 1200 );
     TCanvas* canvDeltaBase = new TCanvas( "canvDeltaBase", "canvDeltaBase", 900, 600 );
     TCanvas* canvDeltaCharge = new TCanvas( "canvDeltaCharge", "", 900, 600 );
     TCanvas* canvBaseCH = new TCanvas( "canvBaseCH", "", 1300, 600 );
+    TCanvas* canvPeak = new TCanvas( "canvPeak", "", 1300, 600 );
     
     TH1D* histoBase = new TH1D( "baseline", "Mean Baseline", 18, -0.5, 17.5 );
+    TH1D* histoPeak = new TH1D( "peak", "Peak", 18, -0.5, 17.5);
+=======
+
+    TCanvas* c1bis = new TCanvas( "c1bis", "multipads", 600, 600 );
+    TCanvas* canvcharge = new TCanvas( "canvcharge", "canvcharge", 600, 600 );
+    TCanvas* canvDeltaBase = new TCanvas( "canvDeltaBase", "canvDeltaBase", 600, 600 );
+    TCanvas* canvDeltaCharge = new TCanvas( "canvDeltaCharge", "", 600, 600 );
+    TCanvas* canvBaseCH = new TCanvas( "canvBaseCH", "canvBaseCH", 1200, 600 );
     
+    TH1D* histoBase = new TH1D( "baseline", "baseline", 18, -1.5, 16.5 );
+    
+>>>>>>> parent of 6899827... Merge branch 'master' of https://github.com/gioacchinop/Sfera
     
     c1bis->Divide(1,4);
     std::vector<TH1D> hchBase, charge, deltaCharge;
     std::vector<TLegend> legend;
+    
+    double peak[16];
     
     size_t pos = 0;
     std::string prefix;
@@ -73,10 +90,11 @@ void DataTree::Loop(std::string filename)
     }
     std::string plotsDir( Form( "plots/%s", prefix.c_str() ) );
     std::string chargesDir( Form( "plots/%s/charges", prefix.c_str() ) );
-
+    
     system( Form( "mkdir -p %s", plotsDir.c_str() ) );
     system( Form( "mkdir -p %s", chargesDir.c_str() ) );
-
+<<<<<<< HEAD
+    
     hchBase.push_back(TH1D( "hchBase1", "" , 100,- 1, 1 ));
     hchBase.push_back(TH1D( "hchBase2", "", 100,- 1, 1 ));
     hchBase.push_back(TH1D( "hchBase3", "", 100,- 1, 1 ));
@@ -93,7 +111,7 @@ void DataTree::Loop(std::string filename)
     hchBase.push_back(TH1D( "hchBase14", "", 100,- 1, 1 ));
     hchBase.push_back(TH1D( "hchBase15", "", 100,- 1, 1 ));
     hchBase.push_back(TH1D( "hchBase16", "", 100,- 1, 1 ));
-
+    
     charge.push_back(TH1D( "hcharge1", "", 80, 0, 60 ));
     charge.push_back(TH1D( "hcharge2", "", 80, 0, 60 ));
     charge.push_back(TH1D( "hcharge3", "", 80, 0, 60 ));
@@ -110,7 +128,7 @@ void DataTree::Loop(std::string filename)
     charge.push_back(TH1D( "hcharge14", "", 80, 0, 60 ));
     charge.push_back(TH1D( "hcharge15", "", 80, 0, 60 ));
     charge.push_back(TH1D( "hcharge16", "", 80, 0, 60 ));
-
+    
     deltaCharge.push_back(TH1D( "hch1dc", "", 100, 0.048, 0.058 ));
     deltaCharge.push_back(TH1D( "hch2dc", "", 100, 0.048, 0.058 ));
     deltaCharge.push_back(TH1D( "hch3dc", "", 100, 0.048, 0.058 ));
@@ -127,13 +145,73 @@ void DataTree::Loop(std::string filename)
     deltaCharge.push_back(TH1D( "hch14dc", "", 100, 0.048, 0.058 ));
     deltaCharge.push_back(TH1D( "hch15dc", "", 100, 0.048, 0.058 ));
     deltaCharge.push_back(TH1D( "hch16dc", "", 100, 0.048, 0.058 ));
-
+    
     
     legend.push_back(TLegend(0.83,0.6,0.9,0.8));
     legend.push_back(TLegend(0.83,0.6,0.9,0.8));
     legend.push_back(TLegend(0.83,0.6,0.9,0.8));
     legend.push_back(TLegend(0.83,0.6,0.9,0.8));
+    
+=======
 
+    hchBase.push_back(TH1D( "hchBase1", "", 100,- 0.001, 0.001 ));
+    hchBase.push_back(TH1D( "hchBase2", "", 100,- 0.001, 0.001 ));
+    hchBase.push_back(TH1D( "hchBase3", "", 100,- 0.001, 0.001 ));
+    hchBase.push_back(TH1D( "hchBase4", "", 100,- 0.001, 0.001 ));
+    hchBase.push_back(TH1D( "hchBase5", "", 100,- 0.001, 0.001 ));
+    hchBase.push_back(TH1D( "hchBase6", "", 100,- 0.001, 0.001 ));
+    hchBase.push_back(TH1D( "hchBase7", "", 100,- 0.001, 0.001 ));
+    hchBase.push_back(TH1D( "hchBase8", "", 100,- 0.001, 0.001 ));
+    hchBase.push_back(TH1D( "hchBase9", "", 100,- 0.001, 0.001 ));
+    hchBase.push_back(TH1D( "hchBase10", "", 100,- 0.001, 0.001 ));
+    hchBase.push_back(TH1D( "hchBase11", "", 100,- 0.001, 0.001 ));
+    hchBase.push_back(TH1D( "hchBase12", "", 100,- 0.001, 0.001 ));
+    hchBase.push_back(TH1D( "hchBase13", "", 100,- 0.001, 0.001 ));
+    hchBase.push_back(TH1D( "hchBase14", "", 100,- 0.001, 0.001 ));
+    hchBase.push_back(TH1D( "hchBase15", "", 100,- 0.001, 0.001 ));
+    hchBase.push_back(TH1D( "hchBase16", "", 100,- 0.001, 0.001 ));
+
+    charge.push_back(TH1D( "hcharge1", "", 100, 0, 60 ));
+    charge.push_back(TH1D( "hcharge2", "", 100, 0, 60 ));
+    charge.push_back(TH1D( "hcharge3", "", 100, 0, 60 ));
+    charge.push_back(TH1D( "hcharge4", "", 100, 0, 60 ));
+    charge.push_back(TH1D( "hcharge5", "", 100, 0, 60 ));
+    charge.push_back(TH1D( "hcharge6", "", 100, 0, 60 ));
+    charge.push_back(TH1D( "hcharge7", "", 100, 0, 60 ));
+    charge.push_back(TH1D( "hcharge8", "", 100, 0, 60 ));
+    charge.push_back(TH1D( "hcharge9", "", 100, 0, 60 ));
+    charge.push_back(TH1D( "hcharge10", "", 100, 0, 60 ));
+    charge.push_back(TH1D( "hcharge11", "", 100, 0, 60 ));
+    charge.push_back(TH1D( "hcharge12", "", 100, 0, 60 ));
+    charge.push_back(TH1D( "hcharge13", "", 100, 0, 60 ));
+    charge.push_back(TH1D( "hcharge14", "", 100, 0, 60 ));
+    charge.push_back(TH1D( "hcharge15", "", 100, 0, 60 ));
+    charge.push_back(TH1D( "hcharge16", "", 100, 0, 60 ));
+
+    deltaCharge.push_back(TH1D( "hch1dc", "", 100, 0.045, 0.06 ));
+    deltaCharge.push_back(TH1D( "hch2dc", "", 100, 0.045, 0.06 ));
+    deltaCharge.push_back(TH1D( "hch3dc", "", 100, 0.045, 0.06 ));
+    deltaCharge.push_back(TH1D( "hch4dc", "", 100, 0.045, 0.06 ));
+    deltaCharge.push_back(TH1D( "hch5dc", "", 100, 0.045, 0.06 ));
+    deltaCharge.push_back(TH1D( "hch6dc", "", 100, 0.045, 0.06 ));
+    deltaCharge.push_back(TH1D( "hch7dc", "", 100, 0.045, 0.06 ));
+    deltaCharge.push_back(TH1D( "hch8dc", "", 100, 0.045, 0.06 ));
+    deltaCharge.push_back(TH1D( "hch9dc", "", 100, 0.045, 0.06 ));
+    deltaCharge.push_back(TH1D( "hch10dc", "", 100, 0.045, 0.06 ));
+    deltaCharge.push_back(TH1D( "hch11dc", "", 100, 0.045, 0.06 ));
+    deltaCharge.push_back(TH1D( "hch12dc", "", 100, 0.045, 0.06 ));
+    deltaCharge.push_back(TH1D( "hch13dc", "", 100, 0.045, 0.06 ));
+    deltaCharge.push_back(TH1D( "hch14dc", "", 100, 0.045, 0.06 ));
+    deltaCharge.push_back(TH1D( "hch15dc", "", 100, 0.045, 0.06 ));
+    deltaCharge.push_back(TH1D( "hch16dc", "", 100, 0.045, 0.06 ));
+
+    
+    legend.push_back(TLegend(0.7,0.2,0.9,0.4));
+    legend.push_back(TLegend(0.7,0.2,0.9,0.4));
+    legend.push_back(TLegend(0.7,0.2,0.9,0.4));
+    legend.push_back(TLegend(0.7,0.2,0.9,0.4));
+
+>>>>>>> parent of 6899827... Merge branch 'master' of https://github.com/gioacchinop/Sfera
     TH1D* hDeltaBase = new TH1D( "hDbase", "", 100,- 100, 100 );
     
     gStyle->SetOptStat(1111111);
@@ -143,34 +221,46 @@ void DataTree::Loop(std::string filename)
         Long64_t ientry = LoadTree(jentry);
         if (ientry < 0) break;
         nb = fChain->GetEntry(jentry);   nbytes += nb;
-             
+        
         for(int i = 0; i < nch; i++){ //LOOP OVER CHANNELS
             Event event(ev, i+1, 1024);
-	        baseline = event.baseline(pshape[i],2);
+            baseline = event.baseline(pshape[i],2);
             integral = event.trapezIntegrator(0,1024, pshape[i])-(baseline*1024);
-	    //  std::cout<< "integral : "<< integral << std::endl;
+            //  std::cout<< "integral : "<< integral << std::endl;
             
             //***************** grafico 1 *******************************
-            if(ev == 47 && integral < -2 ){
+            if(ev == 1 && integral < -2 ){
                 event.pShapeHisto(pshape[i], filename.c_str());
             }
             
             //***************** grafico 1 bis *******************************
-            hchBase[i].Fill(1000*baseline);
+            hchBase[i].Fill(baseline);
             
             //***************** grafico delta base. *******************************
-
+<<<<<<< HEAD
+            
             if (i == 0 ) hDeltaBase->Fill((baseline-base[i])/base[i]);
+            
+            //***************** grafico 2 *******************************
+            if (integral<-2 && integral > -200) deltaCharge[i].Fill(integral/vcharge[i]);
+            
+            //***************** grafico 3 *******************************
+            if (integral<-5 && integral > -200){
+                charge[i].Fill((-integral));
+                
+=======
+
+            if (i == 0 && integral < -0.1 ) hDeltaBase->Fill((baseline-base[i])/base[i]);
 
 	        //***************** grafico 2 *******************************
             if (integral<-2 && integral > -200) deltaCharge[i].Fill(integral/vcharge[i]);
             
             //***************** grafico 3 *******************************
-            if (integral<-5 && integral > -200){
-	        charge[i].Fill((-integral));
-               
+            if (integral<-5){
+	    charge[i].Fill((-integral));
+>>>>>>> parent of 6899827... Merge branch 'master' of https://github.com/gioacchinop/Sfera
             }
-
+            
         }
         
         
@@ -179,25 +269,55 @@ void DataTree::Loop(std::string filename)
     //PLOT CHARGE
     for(int i=0;i<nch;i++){
         canvcharge->cd();
+        
+        peak[i]=charge[i].GetMaximumBin();
+        
+        TFitResultPtr r = charge[i].Fit("gaus","S","",(peak[i]-3)*3./4,(peak[i]+6)*3./4);
+        histoPeak->SetBinContent(histoPeak->FindBin(i+1), r->Parameter(1));
+        histoPeak->SetBinError(histoPeak->FindBin(i+1), r->Parameter(2));
+        
         charge[i].Draw();
-        charge[i].SetTitle(Form("Charge distribution ch. %d", i+1));
-        charge[i].SetXTitle("Integral of pShape [V]"); //OOOOOOOOOOO UNITà DELLA CARICA????????????????????????????????????????????????????????????
-        charge[i].SetYTitle("# of events");
         canvcharge->SaveAs( Form( "%s/charge_ch%d.pdf", chargesDir.c_str(), i+1) );
-        canvcharge->Clear();       
+        canvcharge->Clear();
     }
-
+    
+    histoPeak->SetMarkerStyle(kFullCircle);
+    histoPeak->SetMarkerColor(2);
+    
+    histoPeak->GetXaxis()->SetNdivisions(32,0,0);
+    histoPeak->SetStats(0);
+    histoPeak->SetLineColor(4);
+    canvPeak->cd();
+    canvPeak->SetGridy();
+    
+    histoPeak->SetXTitle("# of Channel");
+    histoPeak->SetYTitle("Peak [V]");//OOOOOOOOOOO UNITà DELLA CARICA????????????????????????????????????????????????????????????
+    gStyle->SetErrorX(0);
+    gStyle->SetEndErrorSize(3);
+    histoPeak->Draw("E1");
+    canvPeak->SaveAs( Form( "%s/PeakDistrCH.pdf",plotsDir.c_str()) );
+    
+    
+    
     //PLOT DELTA CHARGE
+<<<<<<< HEAD
+    for(int i=0;i<nch;i++){
+        canvDeltaCharge->cd();
+        //canvDeltaCharge->SetLogy();
+        
+        deltaCharge[i].Draw();
+        deltaCharge[i].SetTitle(Form("Charge's ratio : Integral/WaveCatcher ch. %d", i+1));
+        deltaCharge[i].SetXTitle("(Pshape integr.)/(WC charge) [V/C]"); //OOOOOOOOOOO UNITà DELLA CARICA??????????????????????????????????????????????????????????
+        deltaCharge[i].SetYTitle("# of events");
+        canvDeltaCharge->SaveAs( Form( "%s/deltaCharge_ch%d.pdf", chargesDir.c_str(), i+1) );
+        canvDeltaCharge->Clear();
+=======
     for(int i=0;i<nch;i++){     
       canvDeltaCharge->cd();
-      //canvDeltaCharge->SetLogy();
-
       deltaCharge[i].Draw();
-      deltaCharge[i].SetTitle(Form("Charge's ratio : Integral/WaveCatcher ch. %d", i+1));
-      deltaCharge[i].SetXTitle("(Pshape integr.)/(WC charge) [V/C]"); //OOOOOOOOOOO UNITà DELLA CARICA??????????????????????????????????????????????????????????
-      deltaCharge[i].SetYTitle("# of events");
       canvDeltaCharge->SaveAs( Form( "%s/deltaCharge_ch%d.pdf", chargesDir.c_str(), i+1) );
       canvDeltaCharge->Clear();      
+>>>>>>> parent of 6899827... Merge branch 'master' of https://github.com/gioacchinop/Sfera
     }
     
     //PLOT DELTA BASE
@@ -205,65 +325,78 @@ void DataTree::Loop(std::string filename)
     canvDeltaBase->SetLogy();
     hDeltaBase->Draw();
     canvDeltaBase->SaveAs( Form( "%s/DeltaBase_ch%d.pdf", plotsDir.c_str(),1 ) );
-   
+    
     //PLOT BASELINE CON PUNTI E ERRORI
     for(int i=0;i<nch;i++){
         histoBase->SetBinContent(histoBase->FindBin(i+1), hchBase[i].GetMean());
         histoBase->SetBinError(histoBase->FindBin(i+1), hchBase[i].GetRMS());
     }
     histoBase->SetMarkerStyle(kFullCircle);
+<<<<<<< HEAD
     histoBase->SetMarkerColor(2);
-
-    histoBase->GetXaxis()->SetNdivisions(32,0,0);
-    histoBase->SetStats(0);
-    histoBase->SetLineColor(4);
-    canvBaseCH->cd();
-    canvBaseCH->SetGridy();
     
-    histoBase->SetXTitle("# of Channel");
-    histoBase->SetYTitle("<Baseline> [mV]");
+    histoBase->GetXaxis()->SetNdivisions(32,0,0);
+=======
+    
+>>>>>>> parent of 6899827... Merge branch 'master' of https://github.com/gioacchinop/Sfera
+    histoBase->SetStats(0);
+    
+    canvBaseCH->cd();
+    canvBaseCH->SetGrid();
+    
+    histoBase->SetXTitle("Channel");
+    histoBase->SetYTitle("<Baseline>");
     gStyle->SetErrorX(0);
     gStyle->SetEndErrorSize(3);
     histoBase->Draw("E1");
     canvBaseCH->SaveAs( Form( "%s/BaseAllCH.pdf",plotsDir.c_str()) );
     
     
-
+    
     //PLOT BASELINE
     int it=1;
+<<<<<<< HEAD
     c1bis->SetTitle("Histo. of baselines for every channel");
-
+    
+=======
+>>>>>>> parent of 6899827... Merge branch 'master' of https://github.com/gioacchinop/Sfera
     for(int j = 0; j < 4; j++) {
         c1bis->cd(j+1);
-        gPad-> SetLogy();
-        int i = 1;
+        c1bis->SetLogy();
 
+        int i = 1;
+        
         for(std::vector<TH1D>::iterator h = hchBase.begin()+(j*4); h != hchBase.begin()+(j+1)*4; h++) {
             
             legend[j].AddEntry(&hchBase[it-1],Form("ch %d",it), "l");
+<<<<<<< HEAD
             h->SetXTitle("Baseline [mV]");
             h->SetStats(0);
             h->GetXaxis()->SetLabelSize(0.09);
             h->GetXaxis()->SetTitleSize(0.09);
             h->GetXaxis()->SetTitleOffset(-0.8);
-
+            
             h->GetYaxis()->SetLabelSize(0.06);
-
+            
+=======
+>>>>>>> parent of 6899827... Merge branch 'master' of https://github.com/gioacchinop/Sfera
             h->Draw("same");
             h->SetLineColor(i);
             it++;
             c1bis->Update();
             i++;
-
+            
         }
-        legend[j].SetTextSize(0.06);
         legend[j].Draw();
-
+        
     }
     c1bis->SaveAs( Form( "%s/baseline.pdf", plotsDir.c_str()) );
     delete c1bis;
     delete canvcharge;
     delete canvDeltaBase;
-    delete  canvBaseCH;
+    delete canvBaseCH;
     delete canvDeltaCharge;
 }
+
+
+
